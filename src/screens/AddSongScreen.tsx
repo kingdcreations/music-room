e8be88990a81645a065b12d9b5a893a9e7cca0e5
KingDCreations <mthais.web@gmail.com>
2023-01-06 18:55:08 +0100

    Adding music list

--- src/screens/AddSongScreen.tsx
+++ src/screens/AddSongScreen.tsx
@@ -1,33 +1,79 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native';
 import React, { useContext, useEffect, useState } from 'react';
-import { Button, Icon, Input, ScrollView, Stack, Text } from 'native-base';
+import { AspectRatio, Button, Flex, HStack, Icon, Image, Input, ScrollView, Stack, Text, View, VStack } from 'native-base';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { FirebaseContext } from '../providers/FirebaseContext';
-import Card from '../components/Card';
-import { Room } from '../types/database';
-import PlaylistButton from '../components/PlaylistButton';
+import SpotifyWebApi from "spotify-web-api-node";
 
 export default function AddSongScreen() {
   const [search, setSearch] = useState("")
+  const [tracks, setTracks] = useState([])
 
   const firebase = useContext(FirebaseContext)
-  const [rooms, setRooms] = useState<Room[]>([])
+
+  var client_id = 'fed4ddbb4f654f368d9661a7384a656a';
+  var client_secret = '4a1b01a42deb48838639fed497f898a8';
+
+  var spotifyApi = new SpotifyWebApi();
+
+  fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    body: 'grant_type=client_credentials&client_id=' + client_id + '&client_secret=' + client_secret,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+  })
+    .then(r => r.json())
+    .then(r => {
+      spotifyApi.setAccessToken(r.access_token);
+    })
+
+  const searchTracks = () => {
+    spotifyApi.searchTracks('track:' + search, { limit: 10 }).then(
+      function (data) {
+        console.log(data.body.tracks.items[0].id);
+        
+        setTracks(data.body.tracks.items)
+      },
+      function (err) {
+        console.error(err);
+      }
+    );
+  }
 
   return (
-    <ScrollView contentContainerStyle={styles.container} m={5}>
-      <Card>
-        <Input
-          value={search}
-          onChangeText={setSearch}
-          InputLeftElement={
-            <Icon as={<Ionicons name="search" />} size={5} ml="2" color="muted.400" />
-          }
-          placeholder="What are we listening to ?" />
-        <Stack flexWrap='wrap' w="100%" justifyContent='center' direction='row'>
-          {rooms.map((room, i) => <PlaylistButton room={room} key={i} />)}
-        </Stack>
-      </Card>
-    </ScrollView>
+    <View style={styles.container}>
+      <Input
+        py={3}
+        value={search}
+        onChangeText={setSearch}
+        onEndEditing={searchTracks}
+        InputLeftElement={
+          <Icon as={<Ionicons name="search" />} size={5} ml="3" color="muted.400" />
+        }
+        placeholder="What are we listening to ?" />
+
+      <ScrollView w="100%">
+        <VStack w="100%" alignItems='center' p={4} space='4'>
+          {tracks.map((track, i) => (
+            <TouchableOpacity key={i} onPress={() => addToPlaylist()}>
+              <HStack w="100%" space="4" alignItems='center'>
+                <Image
+                  size='sm'
+                  source={{
+                    uri: track.album.images[0].url
+                  }} alt="Alternate Text" />
+                <VStack flexGrow={1} flexShrink={1}>
+                  <Text bold isTruncated>{track.name}</Text>
+                  <Text isTruncated>{track.artists.map((artist) => artist.name).join(', ')}</Text>
+                </VStack>
+                <Icon as={<Ionicons name="add-circle-outline" />} size={5} ml="3" color="dark.400" />
+              </HStack>
+            </TouchableOpacity>
+          ))}
+        </VStack>
+      </ScrollView>
+    </View>
   );
 }
 
